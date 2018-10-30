ae41700b66fe2a87e7690585ead8552894a4a40e
alstampe <42574791+alstampe@users.noreply.github.com>
2018-10-30 23:57:11 +0100

    app folder

---
+++ app/run.py
@@ -0,0 +1,185 @@
+import json
+import plotly
+import pandas as pd
+import numpy as np
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from flask import Flask
+from flask import render_template, request
+from plotly.graph_objs import Bar, Pie
+from sklearn.externals import joblib
+from sqlalchemy import create_engine
+
+
+app = Flask(__name__)
+
+def tokenize(text):
+    '''
+    Function performs tokenization, the same way as in the train_classifier.py code
+    Input : text
+    Output : clean tokens 
+    '''
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+#load_data
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+#df = pd.read_sql_table('Data', engine)
+#engine = create_engine('sqlite:///DisasterResponse.db')
+df = pd.read_sql_table('DisasterResponseData', engine)
+
+# load model
+model = joblib.load("../models/Classifier.pkl")
+
+
+# index webpage displays cool visuals and receives user input text for model
+@app.route('/')
+@app.route('/index')
+def index():
+    '''
+    Code specifies visualizations for the app
+    In addition to the provided frist plot of genres there are
+    - plot of all categories, in order of dataset given
+    -plot of the 15 categories with the most occorences, sorted by value
+    -pie chart of genres
+    -pie chart of all categories
+    
+    '''
+
+    # extract data needed for visuals
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
+
+    # Show distribution of different category
+    category = list(df.columns[4:])
+    category_counts = []
+    for column_name in category:
+        category_counts.append(np.sum(df[column_name]))
+
+    # extract data exclude related
+    categories = df.iloc[:,4:]
+    categories_mean = categories.mean().sort_values(ascending=False)[0:15]
+    categories_names = list(categories_mean.index)
+
+
+    # create visuals
+    graphs = [
+        {
+            'data': [
+                Bar(
+                    x=genre_names,
+                    y=genre_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of the three Message Genres',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Genre"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=category,
+                    y=category_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of all Message Categories',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Category"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=categories_names,
+                    y=categories_mean
+                )
+            ],
+
+            'layout': {
+                'title': 'Top 15 Message Categories',
+                'yaxis': {
+                    'title': "Percentage"
+                },
+                'xaxis': {
+                    'title': "Categories"
+                }
+            }
+        },
+        {
+            'data': [
+                Pie(
+                    labels=genre_names,
+                    values=genre_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Genres',
+            }
+        },
+        {
+            'data': [
+                Pie(
+                    labels=category,
+                    values=category_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Categoriess',
+            }
+        }
+    ]
+     
+
+    # encode plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # render web page with plotly graphs
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+
+# web page that handles user query and displays model results
+@app.route('/go')
+def go():
+    # save user input in query
+    query = request.args.get('query', '')
+
+    # use model to predict classification for query
+    classification_labels = model.predict([query])[0]
+    classification_results = dict(zip(df.columns[4:], classification_labels))
+
+    # This will render the go.html Please see that file.
+    return render_template(
+        'go.html',
+        query=query,
+        classification_result=classification_results
+    )
+
+
+def main():
+    app.run(host='0.0.0.0', port=3001, debug=True)
+    
+    
+if __name__ == '__main__':
+    main()
